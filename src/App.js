4b3355a478ac3caf242e49310d9255e69a85450e
Bielhsn <gabrielhenrique.hsn@gmail.com>
2024-04-23 19:47:42 -0300

    Criando API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+
+  const [result, setResult] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const handleClick = async () => {
+    const response = await axios.post('http://localhost:3333/api/call', {prompt: prompt});
+
+    setResult(response.data)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <input type="text" name="" id="" onChange={(e) => setPrompt(e.target.value)}/>
+  <button onClick={handleClick}>Enviar</button>
+  <div>
+    Resultado: {result}
+  </div> 
+</div>
   );
 }
 
